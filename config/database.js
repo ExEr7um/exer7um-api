d96b317591f4487f5435d9b9a79ffe04e530db3c
Vasily Kuzin <exer7um@gmail.com>
2022-03-17 16:55:14 +0300

    Модель сообщений, отправка письма на почту

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "exer7um-api"),
-      user: env("DATABASE_USERNAME", "exer7um"),
-      password: env("DATABASE_PASSWORD", "a14Gp3VRT"),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
       ssl: {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
       },
--- src/api/message/controllers/message.js
+++ src/api/message/controllers/message.js
@@ -1,9 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  *  message controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::message.message');
+module.exports = createCoreController("api::message.message", ({ strapi }) => ({
+  async create(ctx) {
+    const { data, meta } = await super.create(ctx);
+    strapi.plugins["email"].services.email.send({
+      to: "exer7um@gmail.com",
+      from: "exer7um@gmail.com",
+      replyTo: data.attributes.email,
+      subject: `${data.attributes.name} — ${new Date().toLocaleString("ru-RU", {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+      })}`,
+      text: data.attributes.message,
+    });
+    return { data, meta };
+  },
+}));
