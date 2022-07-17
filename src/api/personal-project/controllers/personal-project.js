2e3b2465506ace7889fb87580ce6b87c613de845
Vasily Kuzin <exer7um@gmail.com>
2022-07-17 12:34:46 +0300

    Сущность «Личный проект» #62

---
+++ src/api/personal-project/controllers/personal-project.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  personal-project controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::personal-project.personal-project');
---
+++ src/api/personal-project/routes/personal-project.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * personal-project router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::personal-project.personal-project');
